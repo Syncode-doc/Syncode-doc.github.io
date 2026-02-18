f046353ab46296d2f6594f1285078136214a8e84
Synclaire Williams-Ridley <sgwilliamsridley@aggies.ncat.edu>
2026-02-17 23:01:27 -0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+<script>
+
+    let x=5;
+    let operator = "+";
+    let y=3;
+    let result = x+y;
+
+
+    document.write("<table>"); 
+    document.write("<tr><th>Number 1</th><th>Operator</th><th>Number 2</th><th>Result</th></tr>"); 
+    document.write("<tr><td>" + x + "</td><td>" + operator + "</td><td>" + y + "</td><td>" + result + "</td></tr>"); 
+    document.write("</table>"); 
+
+</script>
